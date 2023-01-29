f7d457739e89227fb82739bb3966c1b9dcc1ede5
gracegoh <gracegoh924@gmail.com>
2023-01-29 21:14:10 +0900

    Change url

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const backend_base_url = 'http://127.0.0.1:8000'
-const frontend_base_url = 'http://127.0.0.1:5500/html/'
+const frontend_base_url = 'https://auto-color.shop/html/'
 
 async function handleLogin() {
     const username = document.getElementById("username").value
--- js/sign_in_up.js
+++ js/sign_in_up.js
@@ -1,5 +1,5 @@
 const backend_base_url = 'http://127.0.0.1:8000'
-const frontend_base_url = 'http://127.0.0.1:5500/html/'
+const frontend_base_url = 'https://auto-color.shop/html/'
 
 const signInBtn = document.getElementById("signIn");
 const signUpBtn = document.getElementById("signUp");
